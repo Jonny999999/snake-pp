05289068a7336e7093482cf2f9f231f958a89116
Julia09055x <steinbergerjulia3@gmail.com>
2023-12-14 11:00:48 +0100

    neue Kommentare und Ton mit Windows.h erzeugt (wird abgespielt!), geht nur bei Windows

--- src/menu.c
+++ src/menu.c
@@ -1,6 +1,7 @@
 #include "menu.h"
 #include "game.h"
 #include <stdio.h>
+#include <Windows.h>
 
 
 void showStartScreen(){
@@ -20,13 +21,14 @@ void showLeaderboard(){
     Uint8 *wavBuffer;
 
     //Prüfung, ob Datei geladen wurde
-    if (SDL_LoadWAV("../sounds/rock-cinematic-161648.wav", &wavSpec, &wavBuffer, &wavLength) == NULL) {
+    //Alternativ ("../sounds/rock-cinematic-161648.wav") -> beide Ordner sind angelegt und die Datei befindet sich in beiden Ordnern
+    if (SDL_LoadWAV("../SDL2/sounds/rock-cinematic-161648.wav", &wavSpec, &wavBuffer, &wavLength) == NULL) {
         printf("Die Audiodatei konnte nicht geladen werden: %s\n", SDL_GetError());
         return;
     }
 
     // Spiel die Audiodatei ab
-    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0); //(Standard-Audiogerät,1 für Aufnahme/ 0 für Wiedergabe,Zeiger auf eine SDL_AudioSpec-Struktur,NULL: In diesem Fall wird kein Zeiger auf eine SDL_AudioSpec-Struktur übergeben, die die tatsächlich verwendeten Audio-Eigenschaften speichert,0: allowed_changes ist ein Bitfeld, das Änderungen an den geöffneten Eigenschaften erlaubt)
     SDL_QueueAudio(deviceId, wavBuffer, wavLength);
     SDL_PauseAudioDevice(deviceId, 0);
     printf("Audiodatei wird abgespielt");
@@ -39,6 +41,11 @@ void showLeaderboard(){
     // Aufräumen
     SDL_CloseAudioDevice(deviceId);
     SDL_FreeWAV(wavBuffer);
+
+    printf("\n\aalarm sound mit a\n");
+    printf("\7alarm sound mit 7\n");
+    // Spiele einen System-Sound ab (Exclamation-Sound)
+    MessageBeep(MB_ICONEXCLAMATION);    //nur dieser Ton wird bei mir abgespielt
 
     return;
 }
