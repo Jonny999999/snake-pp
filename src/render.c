778c69f67aa0332221b9ce1ecf5ccab83de9e850
jonny-vb <jonny-vb>
2023-12-19 09:40:39 -0800

    Change file strcture: Add assets folder
    Change file strcture to make creating a release easier:
    - files: move all resources needed by the executable to the folder assets
    - cmake: copy the assets folder to build folder after compilation
    - adjust all filepaths in source files

--- src/render.c
+++ src/render.c
@@ -124,8 +124,8 @@ void renderStartMenu()
     //=========== only first loop ================
     if(ttlStorage.lastTimeStep == 0)
     {
-        ttlStorage.ptrFont_200 = TTF_OpenFont("../fonts/Quirkus.ttf", ttlStorage.fontSize_200);
-        ttlStorage.ptrFont_30 = TTF_OpenFont("../fonts/Quirkus.ttf", ttlStorage.fontSize_30);
+        ttlStorage.ptrFont_200 = TTF_OpenFont("assets/fonts/Quirkus.ttf", ttlStorage.fontSize_200);
+        ttlStorage.ptrFont_30 = TTF_OpenFont("assets/fonts/Quirkus.ttf", ttlStorage.fontSize_30);
 
 
         SDL_Color textColor1 = {255, 0, 255}; // rosa Text
@@ -240,7 +240,7 @@ void renderSettings()
     if(ttlStorage.lastTimeStep == 0)
     {  
       printf("In first loop\n");
-        ttlStorage.ptrFont_20 = TTF_OpenFont("../fonts/Prototype.ttf", ttlStorage.fontSize_20);
+        ttlStorage.ptrFont_20 = TTF_OpenFont("assets/fonts/Prototype.ttf", ttlStorage.fontSize_20);
         SDL_StartTextInput();     // start text input
     }
 
